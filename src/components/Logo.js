2824f4f2782645cf2bb1c4416e0d8dec1f7eb3b3
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-09 15:54:54 +0530

    File added with styling

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logoSvg from '../assets/logo.svg'
+const Logo = () => {
+  return (
+    <Link to='/' 
+    className='absolute top-[1.5rem] left-[1.5rem] [text-decoration:none] text-lg text-cyan flex items-center '
+    >
+        <img src={logoSvg} alt='CryptoCurrency'/>
+        <span>CryptoCurrency</span>
+    </Link>
+  )
+}
+
+export default Logo
