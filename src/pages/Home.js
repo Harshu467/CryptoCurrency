32e1f4c3ace49d07c972425672a9115d0708d824
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-09 15:54:18 +0530

    Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
+
+function Home() {
+  return (
+    <main className='w-full flex flex-col first-letter:content-center items-center 
+    relative text-white font-nunito'>
+      <div className='w-screen h-screen bg-gray-300 fixed -z-10 ' />
+      <Logo/>
+      <Navigation/>
+      <Outlet/>
+    </main>
+  )
+}
+
+export default Home
