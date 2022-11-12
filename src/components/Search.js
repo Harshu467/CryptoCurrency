01e5dff93eb67541cc643ed18f9eece6a24f2690
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-12 18:20:32 +0530

    Search Box added

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,9 @@ import React from 'react'
 import SearchImg from '../assets/search-icon.svg'
 const Search = () => {
   return (
-    <form>
-        <input type='text' name='Search' />
-        <button type='submit' >\
+    <form className="w-96 relative flex items-center ml-7 font-nunito" >
+        <input type='text' className='w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan' placeholder="Search Here" name='Search' />
+        <button type='submit' >
         <img src={SearchImg} alt="Search" />
         </button>
     </form>
