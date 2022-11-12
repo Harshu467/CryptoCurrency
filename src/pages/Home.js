2b77edfd77a89ae02661f5188fd4c79086c66c0d
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-12 14:17:36 +0530

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,21 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Logo from '../components/Logo'
 import Navigation from '../components/Navigation'
+import { CryptoProvider } from '../contexts/CryptoContext'
 
-const Home=()=> {
+const Home = () => {
   return (
-    <main className='w-full flex flex-col first-letter:content-center items-center 
+    <>
+      <CryptoProvider>
+        <main className='w-full flex flex-col first-letter:content-center items-center 
     relative text-white font-nunito'>
-      <div className='w-screen h-screen bg-gray-300 fixed -z-10 ' />
-      <Logo/>
-      <Navigation/>
-      <Outlet/>
-    </main>
+          <div className='w-screen h-screen bg-gray-300 fixed -z-10 ' />
+          <Logo />
+          <Navigation />
+          <Outlet />
+        </main>
+      </CryptoProvider>
+    </>
   )
 }
 
