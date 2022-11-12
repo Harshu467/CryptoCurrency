28735b59738c977b5ecad221ce9df075fe13af4f
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-12 14:17:21 +0530

    Update

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Filter from '../components/Filter'
 import Tables from '../components/Table'
 
 const Crypto = () => {
   return (
     <section
     className='w-[80%] h-full flex flex-end mt-16 mb-24 relative '>
+      <Filter/>
       <Tables/>
       <Outlet/>
     </section>
