a74fceafd5acc81697198a1d28c69b1215187015
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-12 14:17:00 +0530

    Filter feature

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from './Search'
+
+const Filter = () => {
+  return (
+    <div className='w-full h-12 border-2 border-gray-100 rounded-flex items-center justify-between relative' >
+        <Search/>
+        <div>Currency</div>
+        <div>Sorting</div>
+    </div>
+  )
+}
+
+export default Filter
