eb36941c73a9d061ba8bc4ab07bb05ecedeca6ec
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-12 20:28:52 +0530

    Update

--- src/contexts/CryptoContext.js
+++ src/contexts/CryptoContext.js
@@ -5,20 +5,31 @@ export const CryptoContext = createContext({});
 
 export const CryptoProvider = ({children})=>{
     const [CryptoData, setCryptoData] = useState();
+    const [SearchData, setSearchData] = useState();
     const getCryptoData = async ()=>{
         try {
             const data = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d').then(res=>res.json()).then(json=>json)
-            console.log(data)
             setCryptoData(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const getSearchData = async (query)=>{
+        try {
+            const data = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`).then(res=>res.json()).then(json=>json)
+            setSearchData(data)
+            console.log(data)
+            
         } catch (error) {
             console.log(error)
         }
     }
     useLayoutEffect(() => {
         getCryptoData();
+        getSearchData();
     }, [])
     return(
-        <CryptoContext.Provider value={{CryptoData}}>
+        <CryptoContext.Provider value={{CryptoData,SearchData,getSearchData}}>
             {children}
         </CryptoContext.Provider>
 
