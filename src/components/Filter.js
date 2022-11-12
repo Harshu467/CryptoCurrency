be756cd81d12d04e1bb5a14fc9275e364f8d3018
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-12 20:28:25 +0530

    Updated

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,8 @@ import Search from './Search'
 
 const Filter = () => {
   return (
-    <div className='w-full h-12 border-2 border-gray-100 rounded-flex items-center justify-between relative' >
+    <div className='w-full h-12 border-2 border-gray-100 rounded-lg
+    flex items-center justify-between relative' >
         <Search/>
         <div>Currency</div>
         <div>Sorting</div>
