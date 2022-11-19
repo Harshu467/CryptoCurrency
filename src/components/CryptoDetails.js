240a97fa2d05d1198d95ed8d4505e1853d7561dc
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-19 18:35:38 +0530

    Updated

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -5,7 +5,7 @@ import { CryptoContext } from '../contexts/CryptoContext';
 const CryptoDetails = () => {
     let {coinId} = useParams();
     let navigate = useNavigate();
-    let {coinData:data,getCoinData} = useContext(CryptoContext);
+    let {coinData:data,getCoinData,currency} = useContext(CryptoContext);
     useLayoutEffect(() => {
         getCoinData(coinId);
     }, [coinId])
@@ -25,9 +25,24 @@ const CryptoDetails = () => {
                         <h1 className='text-xl capitalize font-medium'>{data.name}</h1>
                         <span className='text-sm py-0.5 mx-2.5 ml-2 bg-cyan text-cyan bg-opacity-25 rounded uppercase'>{data.symbol}</span>
                     </div>
-                    <div>
-
+                    <div className='flex w-full mt-6'>
+                        <div className='flex flex-col w-full'>
+                            <div className='flex justify-between'>
+                                <span className='text-sm capitalize text-gray-100'>Price</span>
+                            </div>
+                            <div className={`text-sm px-1 ml-2 font-medium flex items-center rounded uppercase bg-opacity-25
+                            ${data.market_data.price_change_percentage_24h>0 ? 'bg-green text-green' : 'bg-red text-red'} `
+                        }>
+                                <span>{Number(data.market_data.price_change_percentage_24h).toFixed(2)}%</span>
+                            </div>
+                        </div>
+                        <h2>{new Intl.NumberFormat("en-IN", {
+                        style: "currency",
+                        currency: currency,
+                        maximumFractionDigits:5
+                      }).format(data.market_data.current_price[currency])}</h2>
                     </div>
+                   
                 </div>
                 <div className='flex flex-col w-[55%] h-full pl-3 bg-green'>
                     Right
