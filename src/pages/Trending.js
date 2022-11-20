06f9cf89afa67895b31d69633b085f81ae3ed550
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-20 21:33:15 +0530

    created component

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,4 +1,6 @@
+import { data } from 'autoprefixer';
 import React, { useContext } from 'react'
+import TrendingCoin from '../components/TrendingCoin';
 import {TrendingContext} from '../contexts/TrendingContext'
 const Trending = () => {
   const {trendData} = useContext(TrendingContext);
@@ -7,7 +9,9 @@ const Trending = () => {
     className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
       <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly  border border-gray-100 rounded">
         {
-          trendData && trendData.map(coin=> <h1>{coin.name}</h1>)
+          trendData && trendData.map(coin=>
+            <TrendingCoin key={data.coin_id} data={coin.item} />
+          )
         }
       </div>
     </section>
