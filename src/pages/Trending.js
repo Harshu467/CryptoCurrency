9b5f0c9bbe8f3026069f98868306dfe203e788f6
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-20 18:52:23 +0530

    updated

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -3,7 +3,9 @@ const Trending = () => {
   return (
     <section
     className='w-[80%] h-full flex flex-end mt-16 mb-24 relative flex-col '>
-      Trending
+      <div className="flex flex-col mt-9 border border-gray-100 rounded">
+        Trending
+      </div>
     </section>
   )
 }
