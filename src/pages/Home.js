577b4d4b860eb4f1b57454adddce25e236c3bf07
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-20 18:52:59 +0530

    Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom'
 import Logo from '../components/Logo'
 import Navigation from '../components/Navigation'
 import { CryptoProvider } from '../contexts/CryptoContext'
+import { TrendingProvider } from '../contexts/TrendingContext'
 
 const Home = () => {
   return (
     <>
       <CryptoProvider>
+      <TrendingProvider>
         <main className='w-full flex flex-col first-letter:content-center items-center 
     relative text-white font-nunito'>
           <div className='w-screen h-screen bg-gray-300 fixed -z-10 ' />
@@ -15,6 +17,7 @@ const Home = () => {
           <Navigation />
           <Outlet />
         </main>
+        </TrendingProvider>
       </CryptoProvider>
     </>
   )
