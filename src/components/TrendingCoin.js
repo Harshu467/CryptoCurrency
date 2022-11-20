0218703f96c051b3d09d0a580ad061b6073b8142
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-20 21:54:02 +0530

    Added Navigate

--- src/components/TrendingCoin.js
+++ src/components/TrendingCoin.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const TrendingCoin = ({data,key}) => {
+    let navigate = useNavigate();
+    const getCoinsDetails = (id) =>{
+        navigate(`${id}`)
+    }
   return (
-    <div className='w-[40%] bg-gray-200 mb-12 last:mb-0 rounded-lg p-4 relative cursor-pointer hover:bg-gray-100 hover:bg-opacity-40' >
+    <div className='w-[40%] bg-gray-200 mb-12 last:mb-0 rounded-lg p-4 relative cursor-pointer hover:bg-gray-100 hover:bg-opacity-40' 
+    onClick={getCoinsDetails(data.id)}
+    >
       { data?
       <>
         <h3 className='txt-base flex items-center my-0.5'>
@@ -29,8 +36,13 @@ const TrendingCoin = ({data,key}) => {
         className='w-[35%] h-auto rounded-full absolute top-2/4 -right-12 -translate-y-2/4' />
         </>
         :
-        null
-      }
+        (
+            <div className="w-full  h-full flex justify-center items-center">
+              <div className="w-8 h-8 border-4 border-cyan rounded-full border-b-gray-200 animate-spin"
+                role="status"/>
+              <span className="ml-2">Please Wait...</span>
+            </div>
+          )}
     </div>
   )
 }
