9f32b75cc3277ed7122f2402d14dae5c5874b872
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-21 11:53:54 +0530

    Responsiveness added

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import logoSvg from '../assets/logo.svg'
 const Logo = () => {
   return (
     <Link to='/' 
-    className='absolute top-[1.5rem] left-[1.5rem] [text-decoration:none] text-lg text-cyan flex items-center '
+    className='absolute  [text-decoration:none] text-lg text-cyan flex items-center sm:text-lg sm:left-[1.5rem] sm:top-[1.5rem]'
     >
         <img src={logoSvg} alt='CryptoCurrency'/>
         <span>CryptoCurrency</span>
