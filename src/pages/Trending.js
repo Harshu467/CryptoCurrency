81721d4b28ecb6d5a820678be1afb2df9eeb117a
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-11-21 11:54:09 +0530

    Responsiveness added

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -9,7 +9,7 @@ const Trending = () => {
     <>
     <section
     className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
-      <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly  border border-gray-100 rounded">
+      <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly  border border-gray-100 rounded lg:flex-row">
         {
           trendData && trendData.map(coin=>
             <TrendingCoin key={data.coin_id} data={coin.item} />
